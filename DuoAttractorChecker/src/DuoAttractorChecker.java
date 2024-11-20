daa67376c9607721280fd8cae21d24f8280de36e
Claudio Simonelli <96047566+Loxcudia@users.noreply.github.com>
2024-11-20 16:20:51 +0100

    Add files via upload

--- DuoAttractorChecker/src/DuoAttractorChecker.java
+++ DuoAttractorChecker/src/DuoAttractorChecker.java
@@ -1,24 +1,50 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class DuoAttractorChecker {
     public static boolean hasDuoAttractor(String word, int position, int span) {
         int length = word.length();
-
         if (position < 0 || position >= length || position + span >= length) {
             System.out.println("Intervallo non valido.");
             return false;
         }
-
-        int attractor1 = position;
-        int attractor2 = position + span;
-
         for (int i = 0; i < length - 1; i++) {
-                if (!crossesAttractors( i, i+1, attractor1, attractor2)) {
-                    System.out.println(i);
+
+                if (!crossesAttractors( word, i, i+1, position, span)) {
                     return false;
-                }
             }
+        }
         return true;
     }
-    private static boolean crossesAttractors(int start, int end, int attractor1, int attractor2) {
-        return (start <= attractor1 && attractor1 <= end) || (start <= attractor2 && attractor2 <= end);
+
+    public static Set<Integer> getDuoAttractorsPositions(String word, int span)
+    {
+        int lenght = word.length();
+        Set<Integer> positions = new HashSet<>();
+        for(int p = 0; p < lenght - 1; p++)
+        {
+            if(p + span > lenght)
+                return positions;
+            if(hasDuoAttractor(word, p, span))
+                positions.add(p);
+        }
+        return positions;
+    }
+
+    private static boolean crossesAttractors(String word, int start, int end, int position, int span) {
+        int count = 0;
+        for(int i = position; i <= position + span; i++)
+        {
+            if(word.charAt(i) != word.charAt(start) && word.charAt(i) != word.charAt(end))
+            {
+                if(count < span + 1)
+                    count++;
+                if(count == span + 1)
+                    return false;
+            }
+        }
+        return true;
     }
 }
--- DuoAttractorChecker/src/Main.java
+++ DuoAttractorChecker/src/Main.java
@@ -1,11 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "aabba";
-        int position = 1;
-        int span = 1;
-
+        String word = "abcdabfecdfe";
+        int position = 4;
+        int span = 2;
+        Set<Integer> positions = new HashSet<>();
         boolean result = DuoAttractorChecker.hasDuoAttractor(word, position, span);
+        positions  = DuoAttractorChecker.getDuoAttractorsPositions(word, span);
+        for (Integer p:
+              positions) {
+            System.out.print(p);
+            System.out.print(", ");
+        }
         System.out.println("Esiste un duo-attrattore: " + result);
     }
 
