8b84d371a8244564f0c0cb3972282ace830140c4
Claudio Simonelli <96047566+Loxcudia@users.noreply.github.com>
2024-12-08 18:07:54 +0100

    Add files via upload

--- DuoAttractorChecker/src/DuoAttractorChecker.java
+++ DuoAttractorChecker/src/DuoAttractorChecker.java
@@ -5,30 +5,93 @@ import java.util.Set;
 
 public class DuoAttractorChecker {
     public static boolean hasDuoAttractor(String word, int position, int span) {
+        //dovrebbe avere complessità O(n^2)
         int length = word.length();
-        String couple = new String();
         if (position < 0 || position >= length || position + span >= length) {
             System.out.println("Intervallo non valido.");
             return false;
         }
+        String couple = new String();
+        String attractorSubstring = word.substring(position - 1, position + span);
         for (int i = 0; i < length - 1; i++) {
             couple = word.substring(i, i+1);
-                if (!crossesAttractors( word, couple, position, span)) {
+                if (!attractorSubstring.contains(couple)) {
                     return false;
             }
         }
         return true;
     }
 
-    private static boolean crossesAttractors(String word, String couple, int position, int span) {
-        for(int i = position - 1; i <= position + span; i++)
-        {
-            if(couple.equals(word.substring(i, i+1)))
-                return true;
+    public static boolean hasDuoAttractor2(String word, int position, int span)
+    {
+        int length = word.length();
+        if (position < 0 || position >= length || position + span >= length) {
+            System.out.println("Intervallo non valido.");
+            return false;
         }
-        return false;
+        Set<Character> alphabet = new HashSet<>();
+        for(int i = 0; i < length - 1; i++)
+            alphabet.add(word.charAt(i));
+        //La seguente mappa associa i simboli dell'alfabeto ad un insieme che contiene tutte le posizioni in cui i simboli occorrono nella stringa data inizialmente in input
+        Map<Character, Set<Integer>> characterToCrossPositionsMap = new HashMap<>();
+        //Riempio la mappa:
+        for (Character symbol: alphabet)
+        {
+            Set<Integer> crossPositions = new HashSet<>();
+            for(int i = 0; i < length - 1; i++)
+            {
+                if(word.charAt(i) == symbol)
+                    crossPositions.add(i);
+            }
+            characterToCrossPositionsMap.put(symbol, crossPositions);
+        }
+
+        for (Character symbol: alphabet)
+        {
+            Set<Integer> crossPositions = characterToCrossPositionsMap.get(symbol);
+            Set<Integer> tmpSet = new HashSet<>(); //set che andiamo a riempire con le posizioni appartenenti a crossPositions che sono incluse tra p e position + span
+            int cont1 = 0, cont2 = 0;
+            //La logica dei contatori mi aiuta ad escludere la prima posizione maggiore uguale di position incontrata e allo stesso tempo ad includere la posizione subito dopo quella finale 
+            for(Integer n: crossPositions)
+            {
+                if(n >= position && n <= position + span - 1)
+                {
+                    cont1++;
+                    if(n + 1 > position + span - 1)
+                        cont2++;
+                    if(cont1 > 1)
+                        tmpSet.add(n);
+                }
+                if(cont2 == 1)
+                {
+                    tmpSet.add(n);
+                    cont2--;
+                }
+            }
+
+            for (Integer n: crossPositions)
+            {
+                if(n < position - 1 || n > position + span)
+                {
+                    if(checkIfIntersectIsEmpty(characterToCrossPositionsMap.get(word.charAt(n +1)), tmpSet))
+                        return false;
+                }
+            }
+        }
+        return true;
     }
 
+    private static boolean checkIfIntersectIsEmpty(Set<Integer> set1, Set<Integer> set2)
+    {
+        /*
+        for (Integer n: set1)
+        {
+            System.out.println(n);
+        }
+        */
+        set1.retainAll(set2);
+        return set1.isEmpty();
+    }
     public static Set<Integer> getDuoAttractorsPositions(String word, int span)
     {
         int lenght = word.length();
--- DuoAttractorChecker/src/Main.java
+++ DuoAttractorChecker/src/Main.java
@@ -4,11 +4,11 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         String word = "abcabc";
-        int position = 1;
-        int span = 2;
-        boolean result = DuoAttractorChecker.hasDuoAttractor(word, position, span);
+        int position = 0;
+        int span = 1;
+        boolean result = DuoAttractorChecker.hasDuoAttractor2(word, position, span);
 
-
+/*
         Set<Integer> positions = new HashSet<>();
         positions  = DuoAttractorChecker.getDuoAttractorsPositions(word, span);
         for (Integer p:
@@ -16,7 +16,7 @@ public class Main {
             System.out.print(p);
             System.out.print(", ");
         }
-
+*/
         System.out.println("Esiste un duo-attrattore: " + result);
     }
 
